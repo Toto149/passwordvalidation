50966620b1b2c4fa3834b894b7f087195a2d1963
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-06 10:31:31 +0100

    added test to the functions. Have done it in the wrong order by accident lol.

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,84 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    void hasNumerals_shouldReturnFalse_whenABCdefghIsGiven(){
+        //GIVEN
+        String password = "ABCdefgh";
+        //WHEN
+        boolean actual = Main.hasNumerals(password);
+        //THEN
+        assertFalse(actual);
+    }
+    @Test
+    void hasNumerals_shouldReturnTrue_whenA12BcdefIsGiven(){
+        //GIVEN
+        String password = "A12Bcdef";
+        //WHEN
+        boolean actual = Main.hasNumerals(password);
+        //THEN
+        assertTrue(actual);
+    }
+    @Test
+    void hasBigCharcters_shouldReturnFalse_when1bcd2234IsGiven(){
+        //GIVEN
+        String password = "1bcd2234";
+        //WHEN
+        boolean actual = Main.hasBigCharacters(password);
+        //THEN
+        assertFalse(actual);
+    }
+    @Test
+    void hasBigCharacters_shouldReturnTrue_whenAbce1234IsGiven(){
+        //GIVEN
+        String password = "Abce1234";
+        //WHEN
+        boolean actual = Main.hasBigCharacters(password);
+        //THEN
+        assertTrue(actual);
+    }
+    @Test
+    void hasSmallCharacters_shouldReturnFalse_whenABC12345IsGiven(){
+        //GIVEN
+        String password = "ABC12345";
+        //WHEN
+        boolean actual = Main.hasSmallCharacters(password);
+        //THEN
+        assertFalse(actual);
+    }
+    @Test
+    void hasSmallCharacters_shouldReturnTrue_whenabcD123IsGiven(){
+        //GIVEN
+        String password = "abcD123";
+        //WHEN
+        boolean actual = Main.hasSmallCharacters(password);
+        //THEN
+        assertTrue(actual);
+    }
+    @Test
+    void hasAppropriateLength_shouldReturnFalse_whenAbc123(){
+        //GIVEN
+        String password = "Abc123";
+        //WHEN
+        boolean actual = Main.hasAppropriateLength(password);
+        //THEN
+        assertFalse(actual);
+    }
+    @Test
+    void hasAppropriateLength_shouldReturnTrue_whenABCD1234(){
+        //GIVEN
+        String password = "ABCD1234";
+        //WHEN
+        boolean actual = Main.hasAppropriateLength(password);
+        //THEN
+        assertTrue(actual);
+    }
+
+
 
 }
